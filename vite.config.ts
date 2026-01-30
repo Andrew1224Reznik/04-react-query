ec3c1cc13bcdf484aec6c798589ddbd0e0e695ca
Andrew1224Reznik <reznik1224@gmail.com>
2026-01-30 20:02:08 +0200

    O

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/04-react-query/",
   build: {
     sourcemap: true,
   },
